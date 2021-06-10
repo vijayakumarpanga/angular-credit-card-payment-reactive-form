5b366dbd7d59b9f662e68ed38510edffb5cbcdf5
VIJAYAKUMAR <pangavijaykumar9966@gmail.com>
2021-06-10 19:12:00 +0530

    add Input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import {FormControl,FormGroup, Validators} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'credit-card-form';
+ cardform = new FormGroup({
+  name : new FormControl('',[Validators.minLength(5),Validators.required]),
+ });
+  title = 'credit-card-form'
+constructor(){
+  console.log(this.cardform);
+  console.log(this.cardform.controls.name)
+  console.log(this.cardform.controls.name.errors)
+}
+
 }
